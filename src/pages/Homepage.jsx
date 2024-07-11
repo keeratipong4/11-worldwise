c597ac0d0cb7f5f91e819252d5e1622514e43d6b
Hybola <keeratipong.b@gmail.com>
2024-07-11 16:40:27 +0700

    lecture 209

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>WorldWise</h1>
-      {/* <a href="/pricing">Pricing</a>  */}
+      <AppNav />
+      <h1 className="test">WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
