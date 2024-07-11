e338584727de845432be295d6b2d9d54a53b8652
Hybola <keeratipong.b@gmail.com>
2024-07-11 15:16:01 +0700

    lecture 207

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+      {/* <a href="/pricing">Pricing</a>  */}
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>NOT FOUND</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
