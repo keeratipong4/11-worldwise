c067a4a384cb2504489cac086b3bb2fd7d63c49d
Hybola <keeratipong.b@gmail.com>
2024-07-14 16:19:02 +0700

    lecture 217

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,7 +11,11 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  console.log(id);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  // console.log(id);
 
   // TEMP DATA
   const currentCity = {
@@ -22,7 +26,14 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City {id} </h1>;
+
+  return (
+    <>
+      <h1>City {id} </h1>
+      <p>Latitude: {lat}</p>
+      <p>Longitude:{lng}</p>
+    </>
+  );
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
