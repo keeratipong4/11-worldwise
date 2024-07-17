ad21d3ac0c1b8c973df4e1a3882ed5f5e05cfe16
Hybola <keeratipong.b@gmail.com>
2024-07-17 15:35:49 +0700

    lecture 229

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import styles from "./Button.module.css";
 function Button({ type, onClick, children }) {
   return (
-    <div onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
       {children}
-    </div>
+    </button>
   );
 }
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -4,6 +4,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
   useEffect(() => {
     async function fetchCities() {
       try {
@@ -20,8 +21,21 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      console.log(err);
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
